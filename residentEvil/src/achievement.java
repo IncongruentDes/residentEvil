26205d20d99f38d42abb91e573da1d41a1164dac
IncongruentDes <132084209+IncongruentDes@users.noreply.github.com>
2023-05-02 16:26:13 -0700

    added achievement class

---
+++ residentEvil/src/achievement.java
@@ -0,0 +1,13 @@
+
+public class achievement {
+	public String name;
+	public Boolean status;
+	public String requirements;
+	
+	
+	public achievement(String Name, Boolean Status, String Req) {
+		name = Name;
+		status = Status;
+		requirements = Req;
+	}
+}
