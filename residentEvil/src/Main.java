7382c5cadcb488151e7eb5eb9c9066f2bb083da1
IncongruentDes <132084209+IncongruentDes@users.noreply.github.com>
2023-05-03 11:15:29 -0700

    edited text files, scanner read in and ArrayList per game creation

--- residentEvil/src/Main.java
+++ residentEvil/src/Main.java
@@ -1,13 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		fileReader("re2r.txt", "Resident Evil 2 Remake");
+		fileReader("re3r.txt", "Resident Evil 3 Remake");
+		fileReader("re4r.txt", "Resident Evil 4 Remake");
 
 	}
 	
-	public void fileReader()
+	public static ArrayList<achievement> fileReader(String text, String games)
 	{
+		
+		// - setting up achievements
 		ArrayList<achievement> game = new ArrayList<achievement>();
+		
+		//-file reading
+		try {
+			Scanner scanner = new Scanner(new File(text));
+
+			while (scanner.hasNextLine()) {
+				String curLine = scanner.nextLine();
+				String[] split = curLine.split("@");
+				achievement curAch = new achievement(split[0].strip(), Boolean.parseBoolean(split[1].strip()), split[2].strip(), games);
+				game.add(curAch);
+			}
+
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		forLoops(game);
+		// - return statement
+		return game;
 	}
+
+	//- easier printing
+	public static void forLoops(ArrayList<?> arrayList) {
+		for (int x = 0; x < arrayList.size(); x++) {
+			System.out.println(arrayList.get(x));
+		}
+	}
+
+
+
 }
--- residentEvil/src/achievement.java
+++ residentEvil/src/achievement.java
@@ -3,11 +3,19 @@ public class achievement {
 	public String name;
 	public Boolean status;
 	public String requirements;
+	String game;
 	
 	
-	public achievement(String Name, Boolean Status, String Req) {
+	public achievement(String Name, Boolean Status, String Req, String Game) {
 		name = Name;
 		status = Status;
 		requirements = Req;
+		game = Game;
+	}
+	
+	public String toString() {
+		String str = name + ": " + requirements + " (" + game + ")";
+		
+		return str;
 	}
 }
